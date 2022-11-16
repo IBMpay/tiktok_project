22fc16b08c63c93e6403d60459884ad06f4e579f
richard <hannemanrichard7@gmail.com>
2022-11-16 23:22:01 +0100

    fix error 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,35 +6,9 @@ import { useWallet } from "@solana/wallet-adapter-react";
 import { useMarket } from "../hooks/useMarket";
 
 const Home: NextPage = () => {
-  const { connected, publicKey } = useWallet();
-  const { mintNft, listNft, delistNft, buyNft } = useMarket();
-
-  const handleMint = () => {
-    mintNft(
-      "my NFT",
-      "https://raw.githubusercontent.com/Coding-and-Crypto/Solana-NFT-Marketplace/master/assets/example.json",
-      1000
-    );
-  };
-
-  const handleList = () => {
-    listNft(100000, 10);
-  };
-
-  const handleDelist = () => {
-    delistNft();
-  };
-
-  const handleBuy = () => {
-    buyNft();
-  };
   return (
     <div>
-      {/* Header */}
-      <button onClick={handleMint}>Mint now</button>
-      <button onClick={handleList}>List nft</button>
-      <button onClick={handleDelist}>Delist nft</button>
-      <button onClick={handleBuy}>Buy nft</button>
+      <p>home page</p>
     </div>
   );
 };
